f83c652b16d932f712e3ea55752af597f81e02f0
helliscloser <mrumais@gmail.com>
2022-07-16 00:05:29 +0530

    Change libs as to miuicamera module
    Changed all libs according to 3 working miuicamera magisk modules.
    
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- config.mk
+++ config.mk
@@ -8,6 +8,8 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiScanner/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiScanner/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/system_ext$(TARGET_COPY_OUT_SYSTEM)/system_ext) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib) \
